39a0a529d428f5a21f5ac83be169af40fdc76c2e
Allen H Roberts <arober49@kennesaw.edu>
2017-05-18 10:59:07 -0400

    initial commit

---
+++ README.txt
@@ -0,0 +1,7 @@
+This script will load each NTUSER.DAT file within C:\users, import the reg file you provide it, then unload the ntuser.dat file.
+
+registry files must match the name of the hive expected by the script. To use do the following.
+
+1. Export from registry or create a new .reg file
+2. Replace the hive name of the registry keys exported (HKEY_CURRENTUSER\RegistryKey will become HKEY_LOCAL_MACHINE\LOADEDHIVE\RegistryKey)
+3. Call the script with the modified .reg file (.\RegistrySynchronizer.ps1 ModifiedRegFile.reg)
---
+++ RegistrySynchronizer.ps1
@@ -0,0 +1,40 @@
+﻿Param (
+    [Parameter(Mandatory=$True)]
+    [string]$Regpath
+)
+
+$profilepath = "C:\users\"
+
+$Profiles = get-childitem $profilepath
+$ntuserprofs = @();
+
+#look through all profiles folders in $profilepath
+foreach($child in $profiles)
+{
+    #look through all children of $profilepath children
+    $items = get-childitem ($profilepath + $child) -Force
+    foreach($item in $items)
+    {
+        #If the folder contains ntuser.dat it adds it to ntuserprofs array.
+        if($item.ToLower() -eq "ntuser.dat")
+        {
+           $ntuserprofs += ,$child 
+        }
+    }
+}
+
+#Load each ntuser.dat and import data.
+foreach($ntuser in $intuserprofs)
+{
+    #Load hive
+    $args = "LOAD HKEY_LOCAL_MACHINE\LOADEDHIVE " + $profilepath + $nutuser.Name + "\ntuser.dat"
+    Start-Process reg.exe $args -Wait
+
+    #edit hive
+    $args = "IMPORT " + $Regpath
+    Start-Process reg.exe $args -Wait
+
+    #unload hive
+    $args = "UNLOAD HKEY_LOCAL_MACHINE\LOADEDHIVE"
+    Start-Process reg.exe $args
+}
